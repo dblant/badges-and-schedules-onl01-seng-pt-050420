f46bec884d8ca9659fbbf36a63c6f308245cbc8a
Dustin Blanton <dustinblntn@gmail.com>
2020-05-12 03:43:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,7 @@ def assign_rooms(speakers)
     greet = []
     speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"
     return greet
-  end
+end
 
 
 
