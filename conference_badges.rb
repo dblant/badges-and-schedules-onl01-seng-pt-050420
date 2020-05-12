1805f75dd5a6c8c01084492f77af10b50cd0209b
Dustin Blanton <dustinblntn@gmail.com>
2020-05-12 03:21:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker (name)
+     p "Hello, my name is #{name}."
+end
+badge_maker("Arel")
+def batch_badge_creator(badge_names)
+   # new_array = []
+    badge_names.map do |names|
+   # new_array <<
+     "Hello, my name is #{names}."
+end
+end
+batch_badge_creator(badge_names)
+rooms = [1,2,3,4,5,6,7]
+def assign_rooms(atendees)
+    #new_array = []
+    badge_names.map do |name,room|
+        "Hello, #{name}! " + "You'll be assigned to room #{room}!"
+end
+assign_rooms(7)
+end
